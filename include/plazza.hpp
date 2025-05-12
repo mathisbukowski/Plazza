400532b654fa6c4c7c36f398b8b2d720f5487e7a
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-05-12 10:30:38 +0200

    chore: adding main

---
+++ include/plazza.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** plazza
+*/
+
+#ifndef PLAZZA_HPP_
+#define PLAZZA_HPP_
+
+#include <iostream>
+
+namespace Plazza {};
+
+#endif /* !PLAZZA_HPP_ */
