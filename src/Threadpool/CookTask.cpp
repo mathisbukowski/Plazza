28148629b6eeba28cce106830f2ce0eb601624ac
Valentin Frappart <valentin.frappart@epitech.eu>
2025-05-12 16:32:25 +0200

    feat: add cook task

---
+++ src/Threadpool/CookTask.cpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** CookTask
+*/
+
+#include "../../include/CookTask.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+
+namespace Plazza {
+    CookTask::CookTask(const Pizza& pizza, double multiplier)
+        : _pizza(pizza), _multiplier(multiplier)
+    {}
+
+    int CookTask::getBaseCookTime(PizzaType type) const
+    {
+        switch (type) {
+            case PizzaType::Regina:
+                return 1;
+            case PizzaType::Margarita:
+                return 2;
+            case PizzaType::Americana:
+                return 2;
+            case PizzaType::Fantasia:
+                return 4;
+            default:
+                return 2;
+        }
+    }
+
+    void CookTask::operator()() const {
+        int baseTime = getBaseCookTime(_pizza.getType());
+        int actualTimeMs = static_cast<int>(baseTime * _multiplier * 1000);
+
+        std::cout << "Cooking pizza (type " << static_cast<int>(_pizza.getType())
+                      << ", size " << static_cast<int>(_pizza.getSize()) << ")...\n";
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(actualTimeMs));
+
+        std::cout << "Pizza cooked!\n";
+
+    }
+}
