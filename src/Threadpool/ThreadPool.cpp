81a42373262ea3f784577a7c48d84fab4b814315
Valentin Frappart <valentin.frappart@epitech.eu>
2025-05-12 15:55:52 +0200

    feat: add threadpool class and implementation

---
+++ src/Threadpool/ThreadPool.cpp
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** ThreadPool
+*/
+
+#include "../../include/ThreadPool.hpp"
+#include <mutex>
+
+namespace Plazza {
+
+    ThreadPool::ThreadPool(size_t threadCount) : _stop(false) {
+        for (size_t i = 0; i < threadCount; ++i)
+            _workers.emplace_back(&ThreadPool::workerLoop, this);
+    }
+
+    ThreadPool::~ThreadPool() {
+        stop();
+        for (auto &worker : _workers) {
+            if (worker.joinable())
+                worker.join();
+        }
+    }
+
+    void ThreadPool::stop() {
+        std::lock_guard<std::mutex> lock(_queueMutex);
+        _stop = true;
+        _condition.notify_all();
+    }
+
+    void ThreadPool::enqueueTask(const std::function<void()>& task) {
+        {
+            std::unique_lock<std::mutex> lock(_queueMutex);
+            _tasks.push(task);
+        }
+        _condition.notify_one();
+    }
+
+    void ThreadPool::workerLoop() {
+        while (!_stop) {
+            std::function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(_queueMutex);
+                _condition.wait(lock, [this] { return _stop || !_tasks.empty(); });
+                if (_stop)
+                    return;
+                task = _tasks.front();
+                _tasks.pop();
+            }
+            task();
+        }
+    }
+
+}
