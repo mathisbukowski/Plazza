e4852ba77ed5e95667489e2e222ed0a3ca54089a
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-05-12 16:15:35 +0200

    feat: adding tools for encapsulation

---
+++ src/Tools/Tools.cpp
@@ -0,0 +1,68 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** Tools
+*/
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <memory>
+
+namespace Plazza {
+    /**
+     * @class Tools
+     * A class that provides encasulation for system calls;
+     */
+    class Tools {
+        public:
+            /**
+             * Forks the current process.
+             * @return The process ID of the child process.
+             */
+            static int toolFork() {
+                int pid = fork();
+
+                if (pid == -1) {
+                    std::cerr << "Fork failed!" << std::endl;
+                    std::exit(EXIT_FAILURE);
+                } else if (pid == 0) {
+                    std::cout << "You are in the child process!" << std::endl;
+                    return 0;
+                }
+                return pid;
+            }
+            /**
+             * Waits for a child process to terminate.
+             * @param pid The process ID of the child process.
+             * @param status A pointer to an integer where the exit status will be stored.
+             * @return The process ID of the terminated child process.
+             */
+            static int toolWaitPid(pid_t pid, std::unique_ptr<int> &status) {
+                
+                int result = waitpid(pid, status.get(), 0);
+                if (result == -1) {
+                    std::cerr << "Waitpid failed!" << std::endl;
+                    return -1;
+                }
+                return result;
+            }
+            /**
+             * Gets the process ID of the current process.
+             * @return The process ID of the current process.
+             */
+            static pid_t toolGetPid() {
+                return getpid();
+            }
+            /**
+             * Gets the process ID of the parent process.
+             * @return The process ID of the parent process.
+             */
+            static pid_t toolGetPPid() {
+                return getppid();
+            }
+
+    };
+}
