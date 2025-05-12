f919e5e67bd401c950a1f3e2e93f0e3287fe145b
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-05-12 14:27:32 +0200

    chore: adding base architecture

---
+++ src/Pizza/PizzaType.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** PizzaType
+*/
+
+#ifndef PIZZATYPE_HPP_
+#define PIZZATYPE_HPP_
+
+namespace Plazza
+{
+    enum PizzaType
+    {
+        Regina = 1,
+        Margarita = 2,
+        Americana = 4,
+        Fantasia = 8
+    };
+    enum PizzaSize
+    {
+        S = 1,
+        M = 2,
+        L = 4,
+        XL = 8,
+        XXL = 16
+    };
+}
+
+#endif /* !PIZZATYPE_HPP_ */
+
