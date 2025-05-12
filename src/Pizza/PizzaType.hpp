ed0d046ffb1213a37c516066719247ca8008e793
Valentin Frappart <valentin.frappart@epitech.eu>
2025-05-12 15:34:13 +0200

    Feat(Pizza):add PizzaClass

--- src/Pizza/PizzaType.hpp
+++ src/Pizza/PizzaType.hpp
@@ -25,7 +25,20 @@ namespace Plazza
         XL = 8,
         XXL = 16
     };
+
+    class Pizza {
+        public:
+            Pizza(PizzaType type, PizzaSize size, int quantity)
+                : _type(type), _size(size), _quantity(quantity) {}
+
+            PizzaType getType() const { return _type; }
+            PizzaSize getSize() const { return _size; }
+            int getQuantity() const { return _quantity; }
+        private:
+            PizzaType _type;
+            PizzaSize _size;
+            int _quantity;
+    };
 }
 
 #endif /* !PIZZATYPE_HPP_ */
-
