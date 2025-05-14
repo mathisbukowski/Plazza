af6331de5d135da74d055b8274524e0b7693066e
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-05-14 12:24:45 +0200

    docs: adding documentation in tools folder

--- src/Tools/ForkEntity.hpp
+++ src/Tools/ForkEntity.hpp
@@ -13,33 +13,83 @@
 #include <string>
 
 namespace Plazza {
+    /**
+     * @class ForkEntity
+     * Class responsible for managing the fork process.
+     */
     class ForkEntity {
     public:
+        /**
+         * Default constructor to initialize the fork entity.
+         */
         ForkEntity();
+        /**
+         * Default destructor to clean up the fork entity.
+         */
         ~ForkEntity() = default;
 
+        /**
+         * Is the current process the parent?
+         * @return True if the current process is the parent, false otherwise
+         */
         [[nodiscard]] bool isParent() const;
+        /**
+         * Is the current process the child?
+         * @return True if the current process is the child, false otherwise
+         */
         [[nodiscard]] bool isChild() const;
 
+        /**
+         * Gets the parent process ID.
+         * @return The parent process ID
+         */
         [[nodiscard]] pid_t getParentPid() const;
+        /**
+         * Gets the child process ID.
+         * @return The child process ID
+         */
         [[nodiscard]] pid_t getChildPid() const;
 
+        /**
+         * Waits for the child process to finish.
+         * @return The exit status of the child process
+         */
         int waitChild();
+        /**
+         * Gets the exit status of the child process.
+         * @return The exit status of the child process
+         */
         [[nodiscard]] std::optional<int> getExitStatus() const;
 
+        /**
+         * @class ForkEntityException
+         * Exception class for handling fork-related errors.
+         * Inherits from std::exception.
+         */
         class ForkEntityException : public std::exception {
         public:
+            /**
+             * Constructor to initialize the exception with a message.
+             * @param message The error message
+             */
             ForkEntityException(const std::string &message): _message(message) {}
+            /**
+             * Destructor for the exception.
+             */
             ~ForkEntityException() = default;
+            /**
+             * Get the error message.
+             * @return The error message
+             */
             [[nodiscard]] const char *what() const noexcept override { return _message.c_str(); }
         private:
-            std::string _message;
+            std::string _message; ///> Error message
         };
 
     private:
-        pid_t _parentPid;
-        pid_t _childPid;
-        std::optional<int> _exitStatus;
+        pid_t _parentPid; ///> Parent process ID
+        pid_t _childPid; ///> Child process ID
+        std::optional<int> _exitStatus; ///> Exit status of the child process
     };
 };
 
--- src/Tools/PosixQueue.hpp
+++ src/Tools/PosixQueue.hpp
@@ -13,18 +13,49 @@
 #include <mqueue.h>
 
 namespace Plazza {
+    /**
+     * @class PosixQueue
+     * Class responsible for managing a POSIX message queue.
+     * @note This class is an encapsulation of the POSIX message queue API from C
+     */
     class PosixQueue {
+        /**
+         * Constructor to initialize the message queue.
+         * @param name The name of the message queue
+         * @param create Flag to indicate if the queue should be created
+         * @param maxMsgSize The maximum size of a message in the queue
+         */
         PosixQueue(const std::string& name, bool create = false, size_t maxMsgSize = 1024);
+        /**
+         * Destructor to clean up the message queue.
+         */
         ~PosixQueue();
 
+        /**
+         * Send a message to the queue.
+         * @param message The message to send
+         * @param priority The priority of the message
+         */
         void send(const std::string& message, unsigned int priority = 0);
+        /**
+         * Receive a message from the queue.
+         * @param priority The priority of the message
+         * @return The received message
+         */
         std::string receive(std::unique_ptr<unsigned int> priority);
+
     private:
-        std::string _name;
-        mqd_t _queue;
-        size_t _maxMsgSize;
-        bool _created;
+        std::string _name; ///> Name of the message queue
+        mqd_t _queue; ///> Message queue descriptor
+        size_t _maxMsgSize; ///> Maximum size of a message in the queue
+        bool _created; ///> Flag to indicate if the queue was created
+        /**
+         * Close the message queue.
+         */
         void closeQueue();
+        /**
+         * Unlink the message queue.
+         */
         void unlinkQueue();
     };
 }
