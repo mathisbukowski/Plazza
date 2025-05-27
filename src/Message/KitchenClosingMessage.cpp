3af4b90d6c768acbcf4cb26a25316091c733a68a
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-05-28 00:38:57 +0200

    feat: add KitchenClosing message implementation and update Pipe interface for IMessage

---
+++ src/Message/KitchenClosingMessage.cpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** 05
+*/
+
+#include "KitchenClosingMessage.hpp"
+
+namespace Plazza {
+    void KitchenClosing::serialize(std::string& buffer) const
+    {
+        buffer.clear();
+        buffer.push_back(static_cast<char>(_type));
+        buffer.push_back(static_cast<char>(_id));
+    }
+
+    void KitchenClosing::deserialize(const std::string& buffer)
+    {
+        if (buffer.size() < 2) {
+            std::cerr << "buffer size is less than 2" << std::endl;
+            return;
+        }
+        _type = static_cast<char>(buffer[0]);
+        _id = static_cast<char>(buffer[1]);
+    }
+}
---
+++ src/Message/KitchenClosingMessage.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2025
+** Plazza
+** File description:
+** 05
+*/
+
+#ifndef KITCHENCLOSINGMESSAGE_HPP
+#define KITCHENCLOSINGMESSAGE_HPP
+#include "IMessage.hpp"
+
+namespace Plazza {
+    class KitchenClosing : IMessage {
+    public:
+        KitchenClosing(uint8_t type): _type(type) {};
+
+        ~KitchenClosing() override = default;
+
+        void serialize(std::string& buffer) const override;
+
+        void deserialize(const std::string& buffer) override;
+
+        uint8_t getType() const override { return _type; }
+
+        uint8_t _type;
+        uint8_t _id = 0;
+    };
+}
+
+
+#endif //KITCHENCLOSINGMESSAGE_HPP
